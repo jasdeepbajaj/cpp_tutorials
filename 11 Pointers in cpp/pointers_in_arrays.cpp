2e098d343d9054ad80e1ad7b74e1a87eb36292d8
jasdeepbajaj <99355859+jasdeepbajaj@users.noreply.github.com>
2024-02-13 00:38:20 -0600

    Changes to be committed: 	new file:   11 Pointers in cpp/pointers_in_arrays.cpp 	new file:   11 Pointers in cpp/pointers_in_arrays.exe

---
+++ 11 Pointers in cpp/pointers_in_arrays.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[] = {10, 20, 30, 40}; // Initialize an array of integers
+    
+    cout << arr << endl; // Outputs the memory address of the first element in the array
+    cout << *arr << endl; // Dereferencing the array name gives the first element of the array (10)
+
+    int *ptr = arr; // Creates a pointer that points to the first element of the array
+
+    // Loop through the first three elements of the array
+    //using pointers to print all the elements in an array
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<ptr<<endl; //outputs the memory location to the currenty elemnet pointed to by pointer
+        cout << *ptr << endl; // Outputs the current element pointed to by ptr
+        ptr++; // Increments the pointer to point to the next element in the array
+    }
+
+    return 0;
+}
