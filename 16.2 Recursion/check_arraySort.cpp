8ba7af3cd4bd540497a35683f34f26b4aaa2e13e
jasdeepbajaj <99355859+jasdeepbajaj@users.noreply.github.com>
2024-04-15 09:28:12 -0500

    Changes to be committed: 	modified:   16.2 Recursion/check_arraySort.cpp 	new file:   16.2 Recursion/check_arraySort.exe 	modified:   8.1 Arrays/maxmin.exe

--- 16.2 Recursion/check_arraySort.cpp
+++ 16.2 Recursion/check_arraySort.cpp
@@ -1,22 +1,37 @@
-#include <iostream>
+#include<iostream>
+#include<climits>
 using namespace std;
 
+// Function to check if an array is sorted in non-decreasing order
+bool sorted(int arr[], int size){
 
-bool sorted(int array[], int size) {
-    if (size == 0)
+    // Base condition: if the size of the array is 1, it is considered sorted
+    if (size == 1)
     {
         return true;
     }
     
-    
+    // Recursive call to check if the rest of the array is sorted
+    bool restArray = sorted(arr+1, size-1);
+
+    // Check if the current element is less than the next element and the rest of the array is sorted
+    if (arr[0] < arr[1] && restArray) 
+    {
+        return true; // If condition met, return true indicating sorted
+    }
+    else
+    {
+        return false; // Otherwise, return false indicating not sorted
+    }
 }
 
-
 int main() {
-    int arr[] = {1,2,3,4,5};
-    
 
+    // Test array
+    int arr[] = {1,5,3,4,5};
+
+    // Call the sorted function and print the result
     cout << sorted(arr, 5) << endl;
 
-    return 0; // Indicate successful program termination
+    return 0;
 }
